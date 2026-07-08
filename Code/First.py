ac14969b263d590e36d9ecd5954256233c35f098
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-07-09 01:46:19 +0530

    Dataset structure and content

--- Code/First.py
+++ Code/First.py
@@ -10,8 +10,28 @@ from pandas import Series
 
 # print('libraries imported')
 
-train = pd.read_csv('Train_SU63IST.csv')
-test = pd.read_csv('Test_0qrQsBZ.csv')
+train = pd.read_csv(r"C:\Users\HP\OneDrive\Documents\Desktop\Time Series Analysis- ALV\Data\Train_SU63ISt.csv")
+test = pd.read_csv(r"C:\Users\HP\OneDrive\Documents\Desktop\Time Series Analysis- ALV\Data\Test_0qrQsBZ.csv")
+
+
+# Dataset Structure and content
+
+
+train_original = train.copy()
+test_original = test.copy()
+
 print(train.head())
 print('*'*40)
 print(test.head())
+
+print(train.columns)
+print('*'*40)
+print(test.columns)
+
+print(train.dtypes)
+print('*'*40)
+print(test.dtypes)
+
+print(train.shape)
+print('*'*40)
+print(test.shape)
