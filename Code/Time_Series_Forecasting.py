5442ea02e722af2ac394fb54c823fd731328929b
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-07-11 02:15:44 +0530

    Data Split for Train-validation WIP

---
+++ Code/Time_Series_Forecasting.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from pandas import Series
+
+train = pd.read_csv(r"C:\Users\HP\OneDrive\Documents\Desktop\Time Series Analysis- ALV\Data\Train_SU63ISt.csv")
+test = pd.read_csv(r"C:\Users\HP\OneDrive\Documents\Desktop\Time Series Analysis- ALV\Data\Test_0qrQsBZ.csv")
+
