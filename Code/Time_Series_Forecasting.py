c08f8203c1a9c06eecf6b9789e8f5e77d74fe4f1
Yogesh Yadav <yogeshyadavyy8736659@gmail.com>
2026-07-11 23:03:56 +0530

    Modeling Techniques- Moving Average Approach

--- Code/Time_Series_Forecasting.py
+++ Code/Time_Series_Forecasting.py
@@ -27,21 +27,59 @@ valid = train.loc['2014-06-25':'2014-09-25']
 
 '''Naive Approach'''
 
-dd = np.asarray(Train.Count)
-y_hat = valid.copy()
-y_hat['naive'] = dd[len(dd)- 1]
-plt.figure(figsize=(10,6))
-plt.plot(Train.index, Train['Count'], label= 'Train')
-plt.plot(valid.index, valid['Count'], label= 'Valid')
-plt.plot(y_hat.index, y_hat['naive'], label = 'Naive Forecast')
-plt.legend(loc='best')
-plt.title("Naive Forecast")
-plt.show()
+# dd = np.asarray(Train.Count)
+# y_hat = valid.copy()
+# y_hat['naive'] = dd[len(dd)- 1]
+# plt.figure(figsize=(10,6))
+# plt.plot(Train.index, Train['Count'], label= 'Train')
+# plt.plot(valid.index, valid['Count'], label= 'Valid')
+# plt.plot(y_hat.index, y_hat['naive'], label = 'Naive Forecast')
+# plt.legend(loc='best')
+# plt.title("Naive Forecast")
+# plt.show()
 
-# Calculate RMSE(Root Mean Square Error)
+# # Calculate RMSE(Root Mean Square Error)
 
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-rms = sqrt(mean_squared_error(valid.Count, y_hat.naive))
+# rms = sqrt(mean_squared_error(valid.Count, y_hat.naive))
+# print(rms)
+#         # 186.37368520699056
+        
+'''Moving Average'''     
+
+y_hat_avg = valid.copy()
+y_hat_avg['moving_avg_forecast'] = Train['Count'].rolling(10).mean().iloc[-1]       #Average of last 10 points
+
+plt.figure(figsize=(12,7))
+plt.plot(Train['Count'], label= 'Train')
+plt.plot(valid['Count'], label='Valid')
+plt.plot(y_hat_avg['moving_avg_forecast'], label='Moving Average Forecast using 10 observations')
+plt.legend(loc = 'best')
+plt.show()
+
+
+y_hat_avg = valid.copy()
+y_hat_avg['moving_avg_forecast'] = Train['Count'].rolling(20).mean().iloc[-1]           #Average of last 20 obsevations 
+
+plt.figure(figsize=(12,7))
+plt.plot(Train['Count'], label = 'Train')
+plt.plot(valid['Count'], label='Valid')
+plt.plot(y_hat_avg['moving_avg_forecast'], label= 'Moving Average Forecast using 20 observations')
+plt.legend(loc = 'best')
+plt.show()
+
+y_hat_avg = valid.copy()
+y_hat_avg['moving_avg_forecast'] = Train['Count'].rolling(50).mean().iloc[-1]       # Average of last 50
+
+plt.figure(figsize=(12,7))
+plt.plot(Train['Count'], label = 'Train')
+plt.plot(valid['Count'], label='Valid')
+plt.plot(y_hat_avg['moving_avg_forecast'], label = 'Moving Average Forecast using 50 observations')
+plt.legend(loc = 'best')
+plt.show()
+
+rms = sqrt(mean_squared_error(valid.Count, y_hat_avg.moving_avg_forecast))
 print(rms)
-        # 186.37368520699056
+
+        #  186.5773761711873
